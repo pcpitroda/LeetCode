04b907da6b7cee5ef76a0801b53c81f628b1815f
pcpitroda <pcpitroda5@gmail.com>
2026-09-25 22:46:42 +0530

    Time: 689 ms (60.60%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -0,0 +1,19 @@
+WITH first_order AS (    
+    SELECT
+        customer_id,
+        MIN(order_date) AS orderD,
+        MIN(customer_pref_delivery_date) AS deliD
+    FROM delivery
+    GROUP BY customer_id
+)
+SELECT 
+    ROUND(
+        AVG(
+            CASE
+                WHEN f.orderD = f.deliD THEN 1
+                ELSE 0
+            END
+        ) * 100,
+        2
+    ) AS immediate_percentage
+FROM first_order f;
